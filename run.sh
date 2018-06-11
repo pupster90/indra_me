e1c0d23b449bacc3ce40626e3fd671983325d389
pupster90 <m3elliot@eng.ucsd.edu>
2018-06-11 23:46:52 +0000

    created bel ipynb file and CxAssembler documentation file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 
-sudo docker run -it -p 80:8888 -v /home/elliott_matthew_a_t/Documents/indra_me:/root/Documents --name indra pupster90/indra:5
+sudo docker run -it -p 80:8888 -v /home/elliott_matthew_a_t/Documents/indra_me:/root/Documents --name indra pupster90/indra:6
 
 
 
